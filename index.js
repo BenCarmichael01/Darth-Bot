b02ce812708d2b33b5511572b8c3710b88a6f873
Ben Carmichael <benccarmichael@gmail.com>
2021-02-09 00:49:42 +0000

    testing merge

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-
+	/////yadayada testing merging
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
