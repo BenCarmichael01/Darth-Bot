3403e696ec252b183a425e7d84208f2202bb8567
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2021-08-11 21:41:44 +0100

    added kick command and en locales

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,31 +1,29 @@
-const Commando = require('discord.js-commando')
+const Commando = require('discord.js-commando');
+const i18n = require('i18n');
+
+// TODO add guildLocale to per-server db
+i18n.setLocale('en');
 
 module.exports = class KickCommand extends Commando.Command {
-    constructor(client) {
-        super(client, {
-            name: 'kick',
-            group: 'moderation',
-            memberName: 'kick',
-            description: 'Kicks a member from the discord server',
-            clientPermissions: ['KICK_MEMBERS'],
-            userPermissions: ['KICK_MEMBERS'],
-        })
-    }
+	constructor(client) {
+		super(client, {
+			name: 'kick',
+			group: 'moderation',
+			memberName: 'kick',
+			description: i18n.__('moderation.kick.description'),
+			clientPermissions: ['KICK_MEMBERS'],
+			userPermissions: ['KICK_MEMBERS'],
+			args: [{ key: 'member', prompt: i18n.__('moderation.kick.prompt'), type: 'member' }],
+		});
+	}
 
-    async run(message) {
-        const target = message.mentions.users.first()
-        if (!target) {
-            message.reply('Please specify someone to kick')
-            return
-        }
-        const { guild } = message
-
-        const member = guild.members.cache.get(target.id)
-        if (member.kickable) {
-            member.kick()
-            message.reply(`<@${target.id}> user has been kicked`)
-        } else {
-            message.reply(`I cannot kick <@${target.id}>, they are too powerful!`)
-        }
-    }
-}
+	async run(message, args) {
+		const { member } = args;
+		if (member.kickable) {
+			member.kick();
+			message.reply(i18n.__mf('moderation.kick.kicked', { member: member.id }));
+		} else {
+			message.reply(i18n.__mf('moderation.kick.fail', { member: member.id }));
+		}
+	}
+};
