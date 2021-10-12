d94f1c409463e14e1bc44cda5837597d68919e25
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2021-10-12 23:52:42 +0100

    remove command now deletes message after send

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -1,7 +1,9 @@
-const { canModifyQueue, LOCALE } = require("../../util/utils");
-const i18n = require("i18n");
-const Commando = require('discord.js-commando')
+require('module-alias/register');
+const { canModifyQueue, LOCALE, MSGTIMEOUT } = require('@util/utils');
+const i18n = require('i18n');
+const Commando = require('discord.js-commando');
 const path = require('path');
+
 i18n.setLocale(LOCALE);
 
 const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
@@ -10,22 +12,40 @@ module.exports = class removeCommand extends Commando.Command {
 	constructor(client) {
 		super(client, {
 			name: 'remove',
+			aliases: ['rm', 'del'],
 			group: 'music',
 			memberName: 'remove',
-			description: i18n.__("remove.description"),
+			description: i18n.__('remove.description'),
 			guildOnly: 'true',
-			argsType: 'single'
-		})
-	};
+			argsType: 'single',
+		});
+	}
+
 	async run(message, args) {
+		message.delete({ TIMEOUT: MSGTIMEOUT });
 		const queue = message.client.queue.get(message.guild.id);
 
-		if (!queue) return message.channel.send(i18n.__("remove.errorNotQueue")).catch(console.error);
-		if (!canModifyQueue(message.member)) return i18n.__("common.errorNotChannel");
-		if (!args.length) return message.reply(i18n.__mf("remove.usageReply", { prefix: message.client.prefix }));
+		if (!queue) {
+			return message.channel.send(i18n.__('remove.errorNotQueue'))
+				.then((msg) => {
+					msg.delete({ timeout: MSGTIMEOUT });
+				}).catch(console.error);
+		}
+		if (!canModifyQueue(message.member)) {
+			return i18n.__('common.errorNotChannel')
+				.then((msg) => {
+					msg.delete({ timeout: MSGTIMEOUT });
+				}).catch(console.error);
+		}
+		if (!args.length) {
+			return message.reply(i18n.__mf('remove.usageReply', { prefix: message.client.prefix }))
+				.then((msg) => {
+					msg.delete({ timeout: MSGTIMEOUT });
+				}).catch(console.error);
+		}
 
-		//const args = args//.join("");
-		const songs = args.split(",").map((arg) => parseInt(arg));
+		// const args = args//.join("");
+		const songs = args.split(', ').map((arg) => parseInt(arg));
 		let removed = [];
 
 		if (pattern.test(args)) {
@@ -35,16 +55,23 @@ module.exports = class removeCommand extends Commando.Command {
 			});
 
 			queue.textChannel.send(
-				`${message.author} ❌ removed **${removed.map((song) => song.title).join("\n")}** from the queue.`
-			);
-		} else if (!isNaN(args[0]) && args[0] >= 1 && args[0] <= queue.songs.length) {
-			console.log("we got elsed!");
+				`${message.author} ❌ removed **${removed.map((song) => song.title).join('\n')}** from the queue.`
+			).then((msg) => {
+				msg.delete({ timeout: MSGTIMEOUT });
+			}).catch(console.error);
+		} else if (!Number.isNaN(args[0]) && args[0] >= 1 && args[0] <= queue.songs.length) {
+			console.log('we got elsed!');
 			return queue.textChannel.send(
 				`${message.author} ❌ removed **${queue.songs.splice(args[0] - 1, 1)[0].title}** from the queue.`
-			);
+			).then((msg) => {
+				msg.delete({ timeout: MSGTIMEOUT });
+			}).catch(console.error);
 		} else {
-			console.log("we got the last one");
-			return message.reply(i18n.__mf("remove.usageReply", { prefix: message.client.prefix }));
+			console.log('we got the last one');
+			return message.reply(i18n.__mf('remove.usageReply', { prefix: message.client.prefix }))
+				.then((msg) => {
+					msg.delete({ timeout: MSGTIMEOUT });
+				}).catch(console.error);
 		}
 	}
 };
