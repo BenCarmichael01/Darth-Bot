1c9d9bc2c30a3a7a2fc6c2e207c31c915e0ba8d5
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-01-06 23:58:57 +0000

    added args to roll command

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,22 +1,23 @@
-const prefix = 'X';
-module.exports =  {
-			name: 'roll',
-			category: 'fun',
-			// argsType: 'multiple',
-			description: `Gives a random number between the specified values \nEx:${prefix}roll <lowest value> <highest value>`,
+module.exports = {
+	name: 'roll',
+	category: 'fun',
+	// argsType: 'multiple',
+	description: 'Gives a random number between the specified values',
+	expectedArgs: '<lowest value> <highest value>',
+	minArgs: 2,
+	maxArgs: 2,
 
-	callback({message, args}) {
+	callback({ message, args }) {
 		var min = Math.ceil(args[0]);
 		var max = Math.floor(args[1]);
 		if (!min) {
 			var min = 1;
-		};
+		}
 		if (!max) {
 			var max = 10;
-		};
+		}
 
-		const output = Math.floor(Math.random() * (max - min + 1) + min);  // returns a random integer from lowerLimit to upperLimit
+		const output = Math.floor(Math.random() * (max - min + 1) + min); // returns a random integer from lowerLimit to upperLimit
 		message.channel.send(output);
-
-	}
+	},
 };
