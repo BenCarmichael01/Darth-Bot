27df34a21ac6873ae2f5f14329dd8ca67e711469
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-01-08 00:23:34 +0000

    test.js

--- commands/testing/test.js
+++ commands/testing/test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* global __base */
 const { findById } = require(`${__base}include/findById`);
 const ytdl = require('ytdl-core-discord');
@@ -8,9 +9,14 @@ module.exports = {
 	category: 'testing',
 	testOnly: true,
 
-	async callback({ message, guild, instance, client }) {
+	async callback({
+		message,
+		guild,
+		instance,
+		client,
+	}) {
 		try {
-			const output = await ytdl.getInfo('https://www.youtube.com/watch?v=slZdwc4T89k');
+			const output = await ytdl('https://www.youtube.com/watch?v=yDpjI66ae6c');
 			console.log(output);
 		} catch (error) {
 			console.error(error);
@@ -18,3 +24,4 @@ module.exports = {
 		message.delete();
 	},
 };
+// https://www.youtube.com/watch?v=slZdwc4T89k
