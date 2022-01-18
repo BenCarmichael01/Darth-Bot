ce2de910d14fd9a9a2cf69fb8e7e43e2eec5fe2c
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-01-18 00:25:22 +0000

    remove command fix

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -1,6 +1,8 @@
 /* global __base */
-const { canModifyQueue, LOCALE } = require(`${__base}include/utils`);
+const { canModifyQueue, LOCALE, MSGTIMEOUT } = require(`${__base}include/utils`);
+const { npMessage } = require(`${__base}include/npmessage`);
 const i18n = require('i18n');
+const { kindMismatchError } = require('soundcloud-downloader/src/util');
 
 i18n.setLocale(LOCALE);
 
@@ -11,56 +13,81 @@ module.exports = {
 	category: 'music',
 	description: i18n.__('remove.description'),
 	guildOnly: 'true',
-	// argsType: 'single'
 
-	callback({ message, args }) {
+	callback({ message, args, prefix }) {
 		const queue = message.client.queue.get(message.guild.id);
-
+		setTimeout(() => message.delete(), 2_500);
 		if (!queue) {
 			return message.channel
 				.send(i18n.__('remove.errorNotQueue'))
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
 				.catch(console.error);
 		}
 		if (!canModifyQueue(message.member)) {
-			return i18n.__('common.errorNotChannel');
+			return message.channel
+				.send(i18n.__('common.errorNotChannel'))
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
 		}
 		if (!args.length) {
-			return message.reply(
-				i18n.__mf('remove.usageReply', { prefix: message.client.prefix }),
-			);
+			return message
+				.reply(i18n.__mf('remove.usageReply', { prefix }))
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
 		}
 		// const args = args//.join("");
-		const songs = args.split(',').map((arg) => parseInt(arg, 10));
+		const songs = args.map((arg) => parseInt(arg, 10));
 		const removed = [];
 
 		if (pattern.test(args)) {
 			queue.songs = queue.songs.filter((item, index) => {
-				if (songs.find((songIndex) => songIndex - 1 === index)) {
+				if (songs.find((songIndex) => songIndex === index)) {
 					removed.push(item);
+					return false;
 				} else return true;
 			});
-
-			queue.textChannel.send(
-				`${message.author} ❌ removed **${removed
-					.map((song) => song.title)
-					.join('\n')}** from the queue.`,
-			);
+			npMessage({ message, prefix, npSong: queue.songs[0] });
+			message.channel
+				.send(
+					`${message.author} ❌ removed \n**${removed
+						.map((song) => song.title)
+						.join('\n& ')}** \nfrom the queue.`,
+				)
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
 		} else if (
-			!Number.isNaN(args[0])
-			&& args[0] >= 1
-			&& args[0] <= queue.songs.length
+			!Number.isNaN(args[0]) &&
+			args[0] >= 1 &&
+			args[0] <= queue.songs.length
 		) {
-			console.log('we got elsed!');
-			return queue.textChannel.send(
-				`${message.author} ❌ removed **${
-					queue.songs.splice(args[0] - 1, 1)[0].title
-				}** from the queue.`,
-			);
+			message.channel
+				.send(
+					`${message.author} ❌ removed **${
+						queue.songs.splice(args[0] - 1, 1)[0].title
+					}** from the queue.`,
+				)
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
+
+			return npMessage({ message, prefix, npSong: queue.songs[0] });
 		} else {
-			console.log('we got the last one');
-			return message.reply(
-				i18n.__mf('remove.usageReply', { prefix: message.client.prefix }),
-			);
+			message
+				.reply(i18n.__mf('remove.usageReply', { prefix }))
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
+			return npMessage({ message, prefix, npSong: queue.songs[0] });
 		}
 	},
 };
