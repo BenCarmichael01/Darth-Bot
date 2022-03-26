5f923fef66170443df89533a4a2f861bb1f5f3d2
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-03-26 00:21:09 +0000

    playlists fully working but slow

--- commands/music/playlist.js
+++ commands/music/playlist.js
@@ -1,21 +1,13 @@
 /* global __base */
 const { play } = require(`${__base}include/play`);
 const { npMessage } = require(`${__base}include/npmessage`);
-const YouTubeAPI = require('simple-youtube-api');
 const i18n = require('i18n');
 const voice = require('@discordjs/voice');
 const playdl = require('play-dl');
 
-const {
-	YOUTUBE_API_KEY,
-	MAX_PLAYLIST_SIZE,
-	DEFAULT_VOLUME,
-	LOCALE,
-	MSGTIMEOUT,
-} = require(`${__base}/include/utils`);
+const { MAX_PLAYLIST_SIZE, DEFAULT_VOLUME, LOCALE, MSGTIMEOUT } = require(`${__base}/include/utils`);
 
 i18n.setLocale(LOCALE);
-const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 
 module.exports = {
 	name: 'playlist',
@@ -41,8 +33,8 @@ module.exports = {
 		// 		.catch(console.error);
 		// }
 		if (!channel) {
-			return message
-				.reply(i18n.__('playlist.errorNotChannel'))
+			return message.channel
+				.send(i18n.__('playlist.errorNotChannel'))
 				.then((msg) => {
 					setTimeout(() => {
 						msg.delete();
@@ -52,24 +44,24 @@ module.exports = {
 		}
 		const permissions = channel.permissionsFor(message.client.user);
 		if (!permissions.has('CONNECT')) {
-			return message
-				.reply(i18n.__('playlist.missingPermissionConnect'))
+			return message.channel
+				.send(i18n.__('playlist.missingPermissionConnect'))
 				.then((msg) => {
 					setTimeout(() => msg.delete(), MSGTIMEOUT);
 				})
 				.catch(console.error);
 		}
 		if (!permissions.has('SPEAK')) {
-			return message
-				.reply(i18n.__('missingPermissionSpeak'))
+			return message.channel
+				.send(i18n.__('missingPermissionSpeak'))
 				.then((msg) => {
 					setTimeout(() => msg.delete(), MSGTIMEOUT);
 				})
 				.catch(console.error);
 		}
 		if (serverQueue && channel !== message.guild.me.voice.channel) {
-			return message
-				.reply(
+			return message.channel
+				.send(
 					i18n.__mf('play.errorNotInSameChannel', {
 						user: message.client.user,
 					}),
@@ -84,14 +76,10 @@ module.exports = {
 		}
 		message.delete();
 
-		const ytPlaylistPattern = /^.*(list=)([^#&?]*).*/gi;
-		const spotPlaylistPattern = /^https?:\/\/(?:open|play)\.spotify\.com\/playlist\/.+$/i;
-
 		const search = args.join(' ');
 		const url = args[0];
-		const isYtPlaylist = ytPlaylistPattern.test(url);
-
-		const isSpotifyPlaylist = spotPlaylistPattern.test(url);
+		const isSpotify = playdl.sp_validate(url);
+		const isYt = playdl.yt_validate(url);
 
 		const queueConstruct = {
 			textChannel: message.channel,
@@ -103,48 +91,79 @@ module.exports = {
 			playing: true,
 		};
 
-		let videos = [];
+		var searching = await message.channel.send(i18n.__('playlist.searching'));
 
-		if (isYtPlaylist) {
+		var videos = [];
+		var playlistTitle = '';
+		if (isYt === 'playlist') {
 			try {
-				let [playlist] = await playdl.search(search, {
-					source: { youtube: 'playlist' },
-					limit: 1,
-				});
-
-				for (
-					let i = 0;
-					i <= (MAX_PLAYLIST_SIZE ? MAX_PLAYLIST_SIZE : 100) && i < playlist.videos.length;
-					i++
-				) {
-					let video = playlist.videos[i];
+				console.time('ytplay');
+				let playlist = await playdl.playlist_info(url);
+				playlistTitle = playlist.title;
+				await playlist.fetch(MAX_PLAYLIST_SIZE);
+				console.timeEnd('ytplay');
+				let vidInfo = playlist.videos;
+				vidInfo.forEach((video) => {
 					let song = {
 						title: video.title,
 						url: video.url,
-						thumbUrl: video.thumbnails[search.thumbnails.length - 1].url,
+						thumbUrl: video.thumbnails[video.thumbnails.length - 1].url,
 						duration: video.durationInSec,
 					};
 					videos.push(song);
-				}
+				});
+				// for (
+				// 	let i = 0;
+				// 	i <= (MAX_PLAYLIST_SIZE ? MAX_PLAYLIST_SIZE : 100) && i < playlist.videos.length;
+				// 	i++
+				// ) {
+				// 	let video = iterator.next().value;
+				// 	let song = {
+				// 		title: video.title,
+				// 		url: video.url,
+				// 		thumbUrl: video.thumbnails[search.thumbnails.length - 1].url,
+				// 		duration: video.durationInSec,
+				// 	};
+				// 	videos.push(song);
+				// }
+				searching.delete().catch(console.error);
 			} catch (error) {
 				console.error(error);
-				return message.reply(i18n.__('playlist.errorNotFoundPlaylist')).catch(console.error);
+				searching.delete().catch(console.error);
+				return message.channel
+					.send(i18n.__('playlist.errorNotFoundPlaylist'))
+					.then((msg) => {
+						setTimeout(() => msg.delete(), MSGTIMEOUT);
+					})
+					.catch(console.error);
 			}
-		} else if (isSpotifyPlaylist) {
+		} else if (isSpotify === 'playlist') {
 			try {
 				let playlist = await playdl.spotify(url);
-				const tracks = await playlist.all_tracks();
-				// Need possible playlist.fetch() here
-				for (
-					let i = 0;
-					i <= (MAX_PLAYLIST_SIZE ? MAX_PLAYLIST_SIZE : 100) && i < tracks.length;
-					i++
-				) {
+				await playlist.fetch(MAX_PLAYLIST_SIZE);
+				const tracks = await playlist.fetched_tracks.get('1');
+				// await tracks.forEach(async (track) => {
+				// 	let term = track.name.concat(' ', track.artists[0].name);
+				// 	let [search] = await playdl.search(term, {
+				// 		source: { youtube: 'video' },
+				// 		limit: 1,
+				// 	});
+				// 	let song = {
+				// 		title: search.title,
+				// 		url: search.url,
+				// 		thumbUrl: search.thumbnails[search.thumbnails.length - 1].url,
+				// 		duration: search.durationInSec,
+				// 	};
+				// 	videos.push(song);
+				// });
+				console.log('y');
+				for (let i = 0; i <= (MAX_PLAYLIST_SIZE ? MAX_PLAYLIST_SIZE : 20) && i < tracks.length; i++) {
+					console.time('ytSearch');
 					let [search] = await playdl.search(tracks[i].name, {
 						source: { youtube: 'video' },
 						limit: 1,
 					});
-
+					console.timeEnd('ytSearch');
 					let song = {
 						title: search.title,
 						url: search.url,
@@ -153,9 +172,16 @@ module.exports = {
 					};
 					videos.push(song);
 				}
+				searching.delete().catch(console.error);
 			} catch (error) {
 				console.error(error);
-				return message.channel.send(error.message).catch(console.error);
+				searching.delete().catch(console.error);
+				return message.channel
+					.send(error.message)
+					.then((msg) => {
+						setTimeout(() => msg.delete(), MSGTIMEOUT);
+					})
+					.catch(console.error);
 			}
 		} else {
 			try {
@@ -183,12 +209,14 @@ module.exports = {
 					};
 					videos.push(song);
 				}
+				searching.delete().catch(console.error);
 			} catch (error) {
+				searching.delete().catch(console.error);
 				console.error(error);
 				return message.channel
 					.send(error.message)
 					.then((msg) => {
-						msg.delete();
+						setTimeout(() => msg.delete(), MSGTIMEOUT);
 					})
 					.catch(console.error);
 			}
@@ -212,14 +240,17 @@ module.exports = {
 		// 	});
 		if (serverQueue) {
 			serverQueue.songs.push(...videos);
+			npMessage({ message, npSong: serverQueue.songs[0], prefix });
+			message.channel
+				.send(i18n.__mf('playlist.queueAdded', { playlist: playlistTitle, author: message.author }))
+				.then((msg) => {
+					setTimeout(() => msg.delete(), MSGTIMEOUT);
+				})
+				.catch(console.error);
 		} else {
-			console.log('testy');
 			queueConstruct.songs.push(...videos);
 		}
 		// serverQueue ? serverQueue.songs.push(...newSongs) : queueConstruct.songs.push(...newSongs);
-		if (serverQueue) {
-			npMessage({ message, npSong: serverQueue.songs[0], prefix });
-		}
 		if (!serverQueue) {
 			message.client.queue.set(message.guild.id, queueConstruct);
 
@@ -232,7 +263,14 @@ module.exports = {
 						adapterCreator: channel.guild.voiceAdapterCreator,
 					});
 				}
-				console.log(queueConstruct.songs);
+				message.channel
+					.send(
+						i18n.__mf('playlist.queueAdded', { playlist: playlistTitle, author: message.author }),
+					)
+					.then((msg) => {
+						setTimeout(() => msg.delete(), MSGTIMEOUT);
+					})
+					.catch(console.error);
 				play(queueConstruct.songs[0], message, prefix);
 			} catch (error) {
 				console.error(error);
