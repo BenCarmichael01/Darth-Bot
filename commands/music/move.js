88c3c8faa4f5b497c8e178c0ce2dc83c52991972
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-04-15 01:09:29 +0100

    move command now slash

--- commands/music/move.js
+++ commands/music/move.js
@@ -14,66 +14,75 @@ module.exports = {
 	description: i18n.__('move.description'),
 	guildOnly: 'true',
 	usage: i18n.__('move.usagesReply'),
+	testOnly: true,
+	slash: true,
+	options: [
+		{
+			name: 'from',
+			description: i18n.__('move.fromDescription'),
+			type: 'INTEGER',
+			required: true,
+		},
+		{
+			name: 'to',
+			description: i18n.__('move.toDescription'),
+			type: 'INTEGER',
+			required: true,
+		},
+	],
 
-	callback({ message, args, prefix }) {
-		const queue = message.client.queue.get(message.guild.id);
-		setTimeout(() => {
-			message.delete();
-		}, 3_000);
-		if (!queue) {
-			return message
-				.reply(i18n.__('move.errorNotQueue'))
-				.then((msg) => {
-					setTimeout(() => msg.delete(), MSGTIMEOUT);
-				})
-				.catch(console.error);
-		}
-		if (!canModifyQueue(message.member)) return;
+	async callback({ interaction, args, prefix, client }) {
+		try {
+			await interaction.deferReply({ ephemeral: true });
+			const queue = client.queue.get(interaction.guildId);
+			if (!queue) {
+				return interaction.editReply({ content: i18n.__('move.errorNotQueue'), ephemeral: true });
+			}
+			if (!canModifyQueue(interaction.member)) return;
 
-		if (!args.length) {
-			return message
-				.reply(i18n.__mf('move.usagesReply', { prefix }))
-				.then((msg) => {
-					setTimeout(() => msg.delete(), MSGTIMEOUT);
-				})
-				.catch(console.error);
-		}
-		if (Number.isNaN(args[0]) || args[0] < 1) {
-			return message
-				.reply(i18n.__mf('move.usagesReply', { prefix }))
-				.then((msg) => {
-					setTimeout(() => msg.delete(), MSGTIMEOUT);
-				})
-				.catch(console.error);
-		}
+			// if (!args.length) {
+			// 	return message
+			// 		.reply(i18n.__mf('move.usagesReply', { prefix }))
+			// 		.then((msg) => {
+			// 			setTimeout(() => msg.delete(), MSGTIMEOUT);
+			// 		})
+			// 		.catch(console.error);
+			// }
+			if (Number.isNaN(args[0]) || args[0] < 1) {
+				return interaction.editReply({
+					content: i18n.__mf('move.usagesReply', { prefix }),
+					ephemeral: true,
+				});
+			}
 
-		const currentPos = args[0];
-		const newPos = args[1];
-		const song = queue.songs[newPos];
-		if (currentPos > queue.songs.length - 1 || newPos > queue.songs.length - 1) {
-			return message
-				.reply(i18n.__('move.range'))
+			const currentPos = args[0];
+			const newPos = args[1];
+			const song = queue.songs[newPos];
+			if (currentPos > queue.songs.length - 1 || newPos > queue.songs.length - 1) {
+				return interaction.editReply({ content: i18n.__('move.range'), ephemeral: true });
+			}
+			queue.songs = move(queue.songs, currentPos, newPos);
+			npMessage({ interaction, npSong: queue.songs[0], prefix });
+			await interaction.editReply({ content: i18n.__('move.success'), ephemeral: true });
+			interaction
+				.followUp(
+					i18n.__mf('move.result', {
+						author: interaction.member.id,
+						title: song.title,
+						index: newPos,
+					}),
+				)
 				.then((msg) => {
-					setTimeout(() => msg.delete(), MSGTIMEOUT);
+					setTimeout(() => {
+						msg.delete();
+					}, MSGTIMEOUT);
 				})
 				.catch(console.error);
+		} catch (error) {
+			console.error(error);
+			interaction
+				.followUp({ content: 'Sorry, an unexpected error has occured.', ephemeral: true })
+				.catch(console.error);
 		}
-		queue.songs = move(queue.songs, currentPos, newPos);
-		npMessage({ message, npSong: queue.songs[0], prefix });
-		message.channel
-			.send(
-				i18n.__mf('move.result', {
-					author: message.author.id,
-					title: song.title,
-					index: newPos,
-				}),
-			)
-			.then((msg) => {
-				setTimeout(() => {
-					msg.delete();
-				}, MSGTIMEOUT);
-			})
-			.catch(console.error);
-		message.delete();
 	},
 };
