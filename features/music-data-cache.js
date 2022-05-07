152716410059f2249db2e4cde1ef97cbf123d960
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-05-07 23:37:38 +0100

    move db cache and message startup to features

---
+++ features/music-data-cache.js
@@ -0,0 +1,17 @@
+const musicSchema = require('../schemas/musicSchema');
+
+module.exports = async (client) => {
+	const docs = await musicSchema.find();
+	docs.forEach((guild) => {
+		const cache = guild._doc;
+		const id = cache._id;
+		delete cache.__v;
+		delete cache._id;
+		client.db.set(id, cache);
+	});
+	client.emit('dbCached');
+};
+module.exports.config = {
+	displayName: 'Database Cache',
+	dbName: 'MUSIC_DATA_CACHE',
+};
