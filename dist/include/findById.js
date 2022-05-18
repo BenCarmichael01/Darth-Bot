5ebdc7d5d5d8e0d7eb0961c9552dd0f0db0022e0
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-05-18 18:13:39 +0100

    remove dist folder

--- dist/include/findById.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const musicSchema = require(`${__base}schemas/musicSchema`);
-module.exports = {
-    async findById(_id) {
-        const doc = await musicSchema.findById(_id);
-        return doc;
-    },
-};
-//# sourceMappingURL=findById.js.map
--- dist/schemas/musicSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const mongoose = require('mongoose');
-const musicSchema = mongoose.Schema({
-    _id: String,
-    musicChannel: String,
-    playingMessage: String,
-});
-module.exports = mongoose.model('musicData', musicSchema, 'musicData');
-//# sourceMappingURL=musicSchema.js.map
