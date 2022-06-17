16c0f97286bb6d64ddd277e3c962a4b33a49fbec
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-06-18 00:00:33 +0100

    fix missing import

--- commands/music/playlist.js
+++ commands/music/playlist.js
@@ -5,6 +5,7 @@ const i18n = require('i18n');
 const voice = require('@discordjs/voice');
 const playdl = require('play-dl');
 const YouTubeAPI = require('simple-youtube-api');
+const he = require('he');
 
 const { MAX_PLAYLIST_SIZE, DEFAULT_VOLUME, LOCALE } = require(`${__base}/include/utils`);
 const { reply, followUp } = require('../../include/responses');
