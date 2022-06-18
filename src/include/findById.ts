5150ec686f63b2cef57df78b0923d92ca0735e9f
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-06-18 16:36:16 +0100

    format findById

--- src/include/findById.ts
+++ src/include/findById.ts
@@ -1,11 +1,11 @@
 import musicSchema from '../schemas/musicSchema';
 
-	/**
-	 *Takes an id string and returns the corresponding entry from the musicData MongoDB table
-	 * @param {String} _id
-	 * @returns {musicDocument} the DB entry
-	 */
-export default async function findById(_id:string) {
-		const doc = await musicSchema.findById(_id);
-		return doc;
-};
+/**
+ *Takes an id string and returns the corresponding entry from the musicData MongoDB table
+ * @param {String} _id
+ * @returns {musicDocument} the DB entry
+ */
+export default async function findById(_id: string): Promise<any> {
+	const doc = await musicSchema.findById(_id);
+	return doc;
+}
