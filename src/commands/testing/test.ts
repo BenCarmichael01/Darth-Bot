479cb4686b0f5ab25a5c85b06511779f3d6b6b13
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-06-19 23:30:20 +0100

    test now ts

--- src/commands/testing/test.ts
+++ src/commands/testing/test.ts
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* global __base */
 
-module.exports = {
+import { ICommand } from 'wokcommands';
+
+export default {
 	name: 'test',
 	description: 'test',
 	category: 'testing',
 	testOnly: true,
+	slash: true,
 	permissions: ['ADMINISTRATOR'],
 
 	async callback({ message, guild, instance, client }) {},
-};
-
+} as ICommand;
