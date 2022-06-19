ad88b7c86b8933471a5889b29e7dc7881a1ed366
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-06-19 20:25:45 +0100

    clearcmds now ts

---
+++ src/commands/testing/clearcmds.ts
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+
+import discordjs from 'discord.js';
+
+import { Client, CommandInteraction, Guild } from 'discord.js';
+
+module.exports = {
+	name: 'clearcmds',
+	description: 'Deletes all Guild or Global commands for this bot',
+	category: 'testing',
+	testOnly: true,
+	slash: true,
+	permissions: ['ADMINISTRATOR'],
+	options: [
+		{
+			name: 'type',
+			description: 'Delete Global or Guild Commands',
+			type: discordjs.Constants.ApplicationCommandOptionTypes.STRING,
+			choices: [
+				{
+					name: 'Guild',
+					value: 'GUILD',
+				},
+				{
+					name: 'Global',
+					value: 'GLOBAL',
+				},
+			],
+			required: true,
+		},
+	],
+
+	async callback({
+		interaction,
+		guild,
+		client,
+		args,
+	}: {
+		interaction: CommandInteraction;
+		guild: Guild;
+		client: Client;
+		args: string[];
+	}) {
+		if (args[0] === 'GLOBAL') {
+			const commands = await client.application?.commands.fetch();
+			commands?.each(async (command) => {
+				await command.delete();
+			});
+			interaction.reply({ content: 'Deleted all global commands...', ephemeral: true });
+		} else if (args[0] === 'GUILD') {
+			guild.commands.cache.forEach((value, key) => {
+				console.log(value.name);
+				value.delete();
+			});
+			interaction.reply({ content: 'Deleted all guild-based commands...', ephemeral: true });
+		}
+	},
+};
