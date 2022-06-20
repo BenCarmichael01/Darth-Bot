d898b5a6aa82b4cef040bce0d5eb51caa6d0bf6c
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-06-20 17:07:10 +0100

    throw cmd improve description

--- src/commands/testing/throw.ts
+++ src/commands/testing/throw.ts
@@ -2,7 +2,7 @@ import { ICommand } from 'wokcommands';
 
 export default {
 	name: 'throw',
-	description: 'throws unhandled error to crash the bot',
+	description: 'Throws an uncaught exception to crash the bot',
 	category: 'testing',
 	testOnly: true,
 	ownerOnly: true,
