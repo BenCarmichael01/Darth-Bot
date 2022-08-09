d12f031a9cbdad55246d6edbe58f77992c6cd22b
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2022-08-09 21:08:41 +0100

    jump cmd return error message

--- src/commands/music/jump.ts
+++ src/commands/music/jump.ts
@@ -38,7 +38,10 @@ export default {
 
 		if (interaction.member) {
 			var member = interaction.member as GuildMember;
-		} else return; // TODO return error message
+		} else {
+			reply({ interaction, content: i18n.__('common.unknownError'), ephemeral: true });
+			return;
+		}
 
 		if (!canModifyQueue(member)) {
 			return reply({
